17448512e3db24448fc23eeddcea4c55c1a2b827
ahmadiphy <ahmadiphy23@gmail.com>
2021-01-11 15:13:57 +0330

    update and add more dicts

--- Fa2Fi.py
+++ Fa2Fi.py
@@ -1,15 +1,29 @@
-def return_fing(mytxt):
-    mydict={'ا':'a', 'آ':'A', 'ب':'b', 'چ':'ch', 'د':'d',
-                    'ف':'f', 'گ':'g', 'ح':'h', 'ی':'y', 'ج':'j',
-                    'ژ':'j', 'ک':'k', 'خ':'kh', 'ل':'l', 'م':'m',
-                    'ن':'n', 'پ':'p', 'ق':'gh', 'ر':'r', 'س':'s',
-                    'ش':'sh', 'ت':'t', 'و':'v', 'ز':'z', 'غ':'gh',
-                    'ع':'a', 'ه':'h', 'ط':'t', 'ص':'s', 'ث':'s',
-                    'ظ':'z', 'ض':'z', 'ذ':'z', ' ':'', '-':''}
-    text= ''
-    for i in range(len(mytxt)):
-        if mydict.get(mytxt[i], None) != None:
-            text = text + mydict[mytxt[i]]
+from Fa2Fi_dicts import *
+
+
+def return_Fi(Fa_str):
+    Fi_str= ''
+    #
+    if Fa2Fi_dict.get(FA_str[0], None) != None:
+        Fi_str = Fi_str + Fa2Fi_dict_UP[Fa_str[0]]
+    else:
+        Fi_str = Fi_str + Fa_str[0]
+    #
+    for i in range(1, len(Fa_str)):
+        if Fa2Fi_dict.get(FA_str[i], None) != None:
+            Fi_str = Fi_str + Fa2Fi_dict[Fa_str[i]]
         else:
-            text = text + mytxt[i]
-    return str(text)
+            Fi_str = Fi_str + Fa_str[i]
+    return str(Fi_str)
+    
+def Final_Correction(Fi_str):
+    Normal_Chars = '0123456789abcdefghijklmnopqrstuvwxyz -_=+/ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()[]"?.,~'
+    for i in range(len(Fi_str)):
+        this_char = Fi_str[i]
+        this_char_state = False
+        for j in range(len(Normal_Chars)):
+            if this_char == Normal_Chars[j]:
+                this_char_state = True
+        if this_char_state is False:
+            Fi_str[i] = '-'
+    return Fi_str
