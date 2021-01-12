9ab8efd2dd709c8793984adf0099e12cc32de4b1
ahmadiphy <ahmadiphy23@gmail.com>
2021-01-12 22:00:27 +0330

    update functions

--- Fa2Fi.py
+++ Fa2Fi.py
@@ -1,19 +1,36 @@
 from Fa2Fi_dicts import *
 
+def check_up(c):
+    state = False
+    test_str = '123456789/*-+ !@#$%^&*()_[]?><.,;:'
+    for i in range(len(test_str)):
+        if c == test_str[i]:
+            state = True
+            break
+    return state
+
+def return_FaCh(c):
+    result = c
+    if Fa2Fi_dict.get(c, None) != None:
+        result = Fa2Fi_dict[c]
+    return result
+
+def return_FaCh_UP(c):
+    result = c
+    if Fa2Fi_dict_UP.get(c, None) != None:
+        result = Fa2Fi_dict_UP[c]
+    return result
 
 def return_Fi(Fa_str):
     Fi_str= ''
     #
-    if Fa2Fi_dict.get(FA_str[0], None) != None:
-        Fi_str = Fi_str + Fa2Fi_dict_UP[Fa_str[0]]
-    else:
-        Fi_str = Fi_str + Fa_str[0]
+    Fi_str = Fi_str + return_FaCh_UP(Fa_str[0])
     #
     for i in range(1, len(Fa_str)):
-        if Fa2Fi_dict.get(FA_str[i], None) != None:
-            Fi_str = Fi_str + Fa2Fi_dict[Fa_str[i]]
+        if check_up(FA_str[i]):
+            Fi_str = Fi_str + return_FaCh_UP(Fa_str[i])
         else:
-            Fi_str = Fi_str + Fa_str[i]
+            Fi_str = Fi_str + return_FaCh(Fa_str[i])
     return str(Fi_str)
     
 def Final_Correction(Fi_str):
